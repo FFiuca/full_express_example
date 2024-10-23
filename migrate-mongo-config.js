b2f65cc57fb5a53cf22eb8b42385c610c194ac92
FFiuca <fardana.fiuca31@gmail.com>
2024-10-23 19:45:22 +0700

    cp

--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -3,7 +3,7 @@
 const config = {
   mongodb: {
     // TODO Change (or review) the url to your MongoDB:
-    url: "mongodb://localhost:27017",
+    url: process.env.MONGO_URI,
 
     // TODO Change this to your database name:
     databaseName: "lnk",
